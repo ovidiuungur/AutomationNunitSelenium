626dd693366cdb848e264149207be0f462c09908
ovidiuungur <ovi_U_smokey@yahoo.com>
2015-10-05 21:45:49 +0300

    add

---
+++ NunitFrameworkStepByStep/Test1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Drawing.Imaging;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using System.IO;
+
+namespace NUnitSelenium
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+
+        [SetUp]
+        public void SetupTest()
+        {
+        }
+
+        [Test]
+        public void Test_OpeningHomePage()
+        {
+            // Step b - Initiating webdriver
+            IWebDriver driver = new FirefoxDriver();
+            //Step c : Making driver to navigate
+            driver.Navigate().GoToUrl("http://docs.seleniumhq.org/");
+
+            //Step d 
+            IWebElement myLink = driver.FindElement(By.LinkText("Download"));
+            myLink.Click();
+
+            TakeScreenshot (driver,"");
+
+
+            //Step e
+            Console.WriteLine("End of test");
+            driver.Quit();
+
+
+
+        }
+        private int _screenshotCount;
+        private void TakeScreenshot(IWebDriver driver, string fileName = "")
+        {
+
+            var screenshotDriver = driver as ITakesScreenshot;
+
+            if (screenshotDriver == null) return;
+
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var testName = TestContext.CurrentContext.Test.Name;
+
+            // If no name is given it will default to TESTSUITE_TESTCASE_COUNT.png.
+            if (String.IsNullOrWhiteSpace(fileName))
+                fileName = string.Format("{0}_{1}_{2:00}.png", GetType().Name, testName, _screenshotCount);
+
+            _screenshotCount++;
+
+            Console.WriteLine("Taking screenshot.");
+            var screenshot = screenshotDriver.GetScreenshot();
+
+            Console.WriteLine("Saving screenshot {0}.", fileName);
+            screenshot.SaveAsFile(currentDirectory + "\\" + fileName, ImageFormat.Png);
+        }
+    }
+}
+    
---
+++ NunitFrameworkStepByStep/Test2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing.Imaging;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using System.IO;
+
+namespace NUnitSelenium
+{
+    [TestFixture]
+    public class UnitTest2
+    {
+
+        [SetUp]
+        public void SetupTest()
+        {
+        }
+
+        [Test]
+        public void Test2()
+        {
+            // Step b - Initiating webdriver
+            IWebDriver driver = new FirefoxDriver();
+            //Step c : Making driver to navigate
+            driver.Navigate().GoToUrl("http://docs.seleniumhq.org/");
+
+            //Step d 
+            IWebElement myLink = driver.FindElement(By.LinkText("Download"));
+            myLink.Click();
+
+           
+            //Step e
+            Console.WriteLine("End of test");
+            driver.Quit();
+
+
+
+        }
+ 
+    }
+}
